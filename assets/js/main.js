cc95a016fb560d9814ca891dcab1e13744b76ac6
tommaso <scocciolinitommaso@gmail.com>
2021-02-26 15:21:56 +0100

    prima versione

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var nome = prompt("Dimmi il tuo nome");
+console.log(nome);
+
+var cognome = prompt("Dimmi ora il tuo cognome");
+console.log(cognome);
+
+var colore = prompt("Il tuo colore preferito?");
+console.log(colore);
+
+document.getElementById('saluto').innerHTML = "Ciao " + nome + " " +cognome;
+document.getElementById('psw').innerHTML = "Una pessima password generata potrebbe essere " + nome + cognome + colore + "21";
